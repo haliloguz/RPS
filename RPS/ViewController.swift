c904b23bf6bf29a9a400e71a3c76e694a23a57a8
Halil Oğuz <1oguzhalil@gmail.com>
2022-07-26 18:19:43 +0300

    Initial Commit

---
+++ RPS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RPS
+//
+//  Created by Halil Oğuz on 26.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
